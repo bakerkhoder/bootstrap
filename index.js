f016ff6bbf5b414969a86e5570a863a5571ad2a3
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-12 22:15:29 +0300

    adding the test for the first part of number if it is valid

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ const send=document.querySelector(".send-button")
 send.addEventListener("click",()=>{
 const name=document.querySelector("#name").value
 const email=document.querySelector("#email").value.split("@")
-const number=document.querySelector("#number")
+const number=document.querySelector("#number").value
 const message=document.querySelector("textarea")
+const firstpartnumber=number.slice(0,4)
+const secondpartnumber=number.slice(4,11)
+var numtest=false
+function numbertest(){
+ if(firstpartnumber=="+961"){
+  if(number.slice(4,6)=="70"||number.slice(4,6)=="71"||number.slice(4,6)=="78"||number.slice(4,6)=="76"||number.slice(4,6)=="78"){
+    if(number.slice(4,12).length==8){
+      numtest=false
+    }
+    else{
+      numtest=true
+    }
+  }
+  else{
+    numtest=true
+  }
+}
+ else{
+  numtest=true
   
-if(name.length<5 || email[0].length<3 ||email[1].length<5 ){
+ }}
+numbertest()
+if(name.length<5 || email[0].length<3 ||email[1].length<5 ||numtest){
  
     console.log("kaka")
+    
   }
 })
