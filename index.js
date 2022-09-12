c686277f90a7f9be5430c09e69bf5843ce8727ef
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-12 23:09:12 +0300

    adding the text alert if any input is false

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const send=document.querySelector(".send-button")
 send.addEventListener("click",()=>{
+
 const name=document.querySelector("#name").value
 const email=document.querySelector("#email").value.split("@")
 const number=document.querySelector("#number").value
-const message=document.querySelector("textarea")
+const message=document.querySelector("textarea").value.length
 const firstpartnumber=number.slice(0,4)
-const secondpartnumber=number.slice(4,11)
+const textalert=document.querySelector("#textalert")
+var text=""
 var numtest=false
 function numbertest(){
  if(firstpartnumber=="+961"){
-  if(number.slice(4,6)=="70"||number.slice(4,6)=="71"||number.slice(4,6)=="78"||number.slice(4,6)=="76"||number.slice(4,6)=="78"){
+  if(number.slice(4,6)=="70"||number.slice(4,6)=="71"||number.slice(4,6)=="78"||number.slice(4,6)=="76"||number.slice(4,6)=="79"||number.slice(4,6)=="81"){
     if(number.slice(4,12).length==8){
       numtest=false
     }
@@ -36,9 +38,24 @@ function numbertest(){
   
  }}
 numbertest()
-if(name.length<5 || email[0].length<3 ||email[1].length<5 ||numtest){
- 
-    console.log("kaka")
+if(name.length<5){
+ text="you should enter minimun of 5 letters"
+}
+if(email[0].length<3 ||email[1].length<5){
+  text="invalid email"
+}
+if( message<100){
+  text="you must insert a minimun of 100 letters"
+}
+
+textalert.style.backgroundColor=" rgb(64, 170, 147)"
+textalert.textContent="information saved"
+if(name.length<5 || email[0].length<3 ||email[1].length<5 ||numtest || message<100){
+    
+    textalert.textContent=text
+    textalert.style.backgroundColor="red"
+   
+   
     
   }
 })
